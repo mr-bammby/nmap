2e79bed8c0171cc4b74a990572a05c5a9b586fab
Domen Banfi <banfidomen@gmail.com>
2026-07-07 22:53:22 +0200

    rename function multi_thread_shared_init to multi_thread_exec for clarity

--- main/src/multi_thread_exec.c
+++ main/src/multi_thread_exec.c
@@ -1,6 +1,6 @@
 #include "multi_thread_shared.h"
 
-void multi_thread_shared_init(const argparse_params_t *params, scan_result_t **results, uint32_t results_rows, uint32_t results_cols)
+void multi_thread_exec(const argparse_params_t *params, scan_result_t **results, uint32_t results_rows, uint32_t results_cols)
 {
     last_queued_cmd_t last_queued_cmd;
 
