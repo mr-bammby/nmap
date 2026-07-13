4e50b1b10ea7a355a2e63b25fa5ffb00746891f4
Domen Banfi <banfidomen@gmail.com>
2026-07-13 21:09:03 +0200

    Fix return valus in flagging array also added reset function

--- threading/inc/th_flagging_array.h
+++ threading/inc/th_flagging_array.h
@@ -19,10 +19,20 @@ typedef struct
     th_lock_access_t access;
 } th_flagging_array_access_t;
 
+typedef enum th_flagging_array_status
+{
+    TH_FLAGGING_ARRAY_OK_GENERIC = 0,
+    TH_FLAGGING_ARRAY_ERR_GENERIC = -1,
+    TH_FLAGGING_ARRAY_ERR_LOCK = -2,
+    TH_FLAGGING_ARRAY_ERR_INVALID_PARAM = -3
+} th_flagging_array_status_t;
 
-uint8_t th_flagging_array_init(th_flagging_array_t *arr, size_t capacity);
-uint8_t th_flagging_array_init_access(th_flagging_array_access_t *access, th_flagging_array_t *array, th_lock_priority_t priority);
-uint8_t th_flagging_array_get(th_flagging_array_access_t *access, size_t idx, uint8_t *ret);
-uint8_t th_flagging_array_set(th_flagging_array_access_t *access, size_t idx, uint8_t val);
+
+th_flagging_array_status_t th_flagging_array_init(th_flagging_array_t *arr, size_t capacity);
+th_flagging_array_status_t th_flagging_array_init_access(th_flagging_array_access_t *access, th_flagging_array_t *array, th_lock_priority_t priority);
+th_flagging_array_status_t th_flagging_array_get(th_flagging_array_access_t *access, size_t idx, uint8_t *ret);
+th_flagging_array_status_t th_flagging_array_set(th_flagging_array_access_t *access, size_t idx, uint8_t val);
+th_flagging_array_status_t th_flagging_array_reset(th_flagging_array_access_t *access);
+
 
 #endif // TH_FLAGGING_ARRAY_H
--- threading/src/th_flagging_array.c
+++ threading/src/th_flagging_array.c
@@ -3,47 +3,55 @@
 #include "th_flagging_array.h"
 
 /* TO be called before thread create */
-uint8_t th_flagging_array_init(th_flagging_array_t *arr, size_t capcity)
+th_flagging_array_status_t th_flagging_array_init(th_flagging_array_t *arr, size_t capcity)
 {
 
     if (!(arr->array = calloc(capcity, sizeof(uint8_t))))
     {
-        return 1;
+        return TH_FLAGGING_ARRAY_ERR_GENERIC;
     }
 
     arr->size = capcity;
 
-    return 0;
+    return TH_FLAGGING_ARRAY_OK_GENERIC;
 }
 
-uint8_t th_flagging_array_init_access(th_flagging_array_access_t *access, th_flagging_array_t *array, th_lock_priority_t priority)
+th_flagging_array_status_t th_flagging_array_init_access(th_flagging_array_access_t *access, th_flagging_array_t *array, th_lock_priority_t priority)
 {
     access->array = array;
     th_lock_init_access(priority, &(access->array->lock), &(access->access));
-    return 0;
+    return TH_FLAGGING_ARRAY_OK_GENERIC;
 }
 
-uint8_t th_flagging_array_get(th_flagging_array_access_t *access, size_t idx, uint8_t *ret)
+th_flagging_array_status_t th_flagging_array_get(th_flagging_array_access_t *access, size_t idx, uint8_t *ret)
 {
     if (idx >= access->array->size)
     {
-        return 1;
+        return TH_FLAGGING_ARRAY_ERR_INVALID_PARAM;
     }
     th_lock_take(&(access->access));
     *ret = access->array->array[idx];
     th_lock_release(&(access->access));
-    return 0;
+    return TH_FLAGGING_ARRAY_OK_GENERIC;
 }
 
 
-uint8_t th_flagging_array_set(th_flagging_array_access_t *access, size_t idx, uint8_t val)
+th_flagging_array_status_t th_flagging_array_set(th_flagging_array_access_t *access, size_t idx, uint8_t val)
 {
     if (idx >= access->array->size)
     {
-        return 1;
+        return TH_FLAGGING_ARRAY_ERR_INVALID_PARAM;
     }
     th_lock_take(&(access->access));
     access->array->array[idx] = val;
     th_lock_release(&(access->access));
-    return 0;
+    return TH_FLAGGING_ARRAY_OK_GENERIC;
+}
+
+th_flagging_array_status_t th_flagging_array_reset(th_flagging_array_access_t *access)
+{
+    th_lock_take(&(access->access));
+    memset(access->array->array, 0, access->array->size);
+    th_lock_release(&(access->access));
+    return TH_FLAGGING_ARRAY_OK_GENERIC;
 }
