973ead93c1b99b273cc109f81e5f5f04b96ec330
Domen Banfi <banfidomen@gmail.com>
2026-08-02 22:03:42 +0200

    Improved command queue and execution flow

--- main/src/multi_thread_exec.c
+++ main/src/multi_thread_exec.c
@@ -10,7 +10,7 @@
 atomic_bool abort_flag = false;
 
 th_queue_t multi_thread_shared_cmd_queue = {0};
-th_flagging_array_t multi_thread_shared_flagging_array = {0};
+th_flagging_array_t *multi_thread_shared_flagging_array;
 
 
 void multi_thread_exec(const argparse_params_t *params, scan_result_t **results, uint32_t results_rows, uint32_t results_cols)
@@ -48,7 +48,7 @@ void multi_thread_exec(const argparse_params_t *params, scan_result_t **results,
             break;
         }
         args[th_num]->cmd_queue = &multi_thread_shared_cmd_queue;
-        args[th_num]->flagging_array = &multi_thread_shared_flagging_array;
+        args[th_num]->flagging_array = multi_thread_shared_flagging_array;
         args[th_num]->thread_id = th_num;
 
         if ( pthread_create(&thread_list[th_num], NULL, multi_thread_sender, (void *)args[th_num]) != 0)
